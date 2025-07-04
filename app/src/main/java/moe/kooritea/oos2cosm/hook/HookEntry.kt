74c38f992b5483e9ac90cef93d53f482c98f539a
kooritea <ochatokori@gmail.com>
2025-07-04 09:55:39 +0800

    移除了无条件放行读取应用列表权限功能

--- app/src/main/java/moe/kooritea/oos2cosm/hook/HookEntry.kt
+++ app/src/main/java/moe/kooritea/oos2cosm/hook/HookEntry.kt
@@ -7,6 +7,8 @@ import com.highcapable.yukihookapi.hook.factory.encase
 import com.highcapable.yukihookapi.hook.factory.field
 import com.highcapable.yukihookapi.hook.factory.method
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
 import org.luckypray.dexkit.DexKitBridge
 import org.luckypray.dexkit.query.enums.StringMatchType
 import java.lang.reflect.Modifier
@@ -24,34 +26,6 @@ class HookEntry : IYukiHookXposedInit {
 
     @OptIn(LegacyResourcesHook::class)
     override fun onHook() = encase {
-        loadSystem {
-            "com.android.server.pm.PackageManagerServiceExtImpl".toClass().apply {
-                method {
-                    name = "getInstalledPackagesAsUserExt"
-                    paramCount = 1
-                }.hook {
-                    replaceTo(any = null)
-                }
-            }
-            "com.android.server.pm.PackageManagerServiceExtImpl".toClass().apply {
-                method {
-                    name = "getInstalledApplicationsAsUserExt"
-                    paramCount = 1
-                }.hook {
-                    replaceTo(any = null)
-                }
-            }
-        }
-        loadApp(name = "com.oplus.account"){
-            "com.platform.account.base.utils.os.DeviceUtil".toClass().apply {
-                method {
-                    name = "isExp"
-                    emptyParam()
-                }.hook {
-                    replaceTo(any = false)
-                }
-            }
-        }
         loadApp(name = "com.oplus.notificationmanager"){
             "com.oplus.notificationmanager.NotificationBackend".toClass().apply {
                 method {
